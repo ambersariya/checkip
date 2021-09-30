17c8762c1e26f16929d9dbda0ae12d527ff1bb59
Danish Javed <danish.javed@gmail.com>
2021-09-30 11:03:30 +0100

    fix(error handling): trap no connectivity handling

--- src/cli.py
+++ src/cli.py
@@ -6,8 +6,11 @@ from src.check_ip import CheckIp
 @click.command()
 def check_ip():
     """Simple program that tells you your current public IP address."""
-    check_ip = CheckIp()
-    click.echo(check_ip.check())
+    try:
+        check_ip = CheckIp()
+        click.echo(check_ip.check())
+    except:
+        click.echo("Error: no connectivity", err=True, color=True)
 
 
 if __name__ == "__main__":
